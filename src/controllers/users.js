743c43f210341a92dade03e0ad5df85f78813b64
anclark686 <43712089+anclark686@users.noreply.github.com>
2022-08-09 23:53:33 -0400

    cookie

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -56,8 +56,10 @@ const Login = async(req, res) => {
             }
         });
         res.cookie('refreshToken', refreshToken,{
-            
+            domain: 'https://reyaly-books.herokuapp.com',
             httpOnly: true,
+            sameSite: "none",
+            secure: true,
             maxAge: 24 * 60 * 60 * 1000
         });
         res.json({ accessToken });
